2a9c97c7c63371b877fa5c81c65e8fe88e2a1aa5
Preet P. Vadaliya <preetvadaliya.ict18@gmail.com>
2022-07-29 22:12:38 +0530

    gh-page

--- README.md
+++
@@ -1,2 +0,0 @@
-# Edu-Bot-Blockly
-The main source code repository for Edu Bot Blockly system.
--- src/generator/events.js
+++
@@ -1,8 +0,0 @@
-/**
- * @author Preet Vadaliya
- */
-
-Blockly.JavaScript['edu_bot_clap_detection'] = function (block) {
-	let code = `8`;
-	return `${code}${block.getSurroundParent() === null ? '#' : ''}`;
-};
